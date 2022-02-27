250ab4238b624d02fc74aea19a4586a088c0d9e2
Eathan O'Connor <37672460+oconew@users.noreply.github.com>
2022-02-27 19:35:09 +0000

    Version 1.0.1
    - Changed port to be dynamically assigned by Heroku.

--- server.js
+++ server.js
@@ -76,6 +76,6 @@ io.on('connection', (socket) => {
     })
   });
 
-server.listen(port, () => {
+server.listen(process.env.PORT || port, () => {
     console.log(`Example app listening on port ${port}`)
 })
